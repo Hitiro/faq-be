3c312088ae2166b8d8d1f970567844b15f51b4b4
Alberto Hitiro Tsugawa <hitiro@live.com>
2023-02-03 19:43:23 -0300

    feat: criando modulo faq

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { FaqModule } from './faq/faq.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), FaqModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'FAQ ~V.0.1';
   }
 }
