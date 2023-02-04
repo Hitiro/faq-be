d12193a6f2720e1a8ab2e86b7133ca2cf876e29b
Alberto Hitiro Tsugawa <hitiro@live.com>
2023-02-03 23:54:43 -0300

    feat: deploy no railway

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('FAQs')
     .setDescription('Cadastro / Alteração / Listagem e Exclusão de FAQs')
-    .setVersion('1.0')
+    .setVersion('0.1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
